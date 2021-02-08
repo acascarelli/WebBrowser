c4f7297bc6efb17ea02e633e715c2b03cb13100f
Austin Cascarelli <austincascarelli@gmail.com>
2021-02-07 17:12:47 -0800

    Added Toolstrip to the Browser Control, Added Backward and Forward page functionality.
    -Was unable to implement tab features do to an issue with parent class visibility. tabControl is set to public visibility.
    
    -Was unable to restore functionality of navigate hotkey (pressing enter) after toolstrip was moved into a user control. I believe this is related to the parent class visibility issue.

---
+++ WebBrowswer.UI/BrowserBar.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowswer.UI
+{
+    public partial class BrowserBar : UserControl
+    {
+
+        Stack<string> BackLinks = new Stack<string>();
+        Stack<string> ForwardLinks = new Stack<string>();
+
+        public BrowserBar()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This web browser was created as part of Auburn University's Software Construction course CPSC-2712-AO1." +
+                "\nDeveloped by Austin Cascarelli (ajc0109) 2021.");
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate(URLTextBox.Text);
+            BackLinks.Push(URLTextBox.Text);
+            
+        }
+
+        // After adding my toolbar(BrowserBar) to my Browswer(TabbedBrowser). This function stopped working and I was not able to figure out what the problem was.
+        private void URLTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //enter key is down
+                this.webBrowser1.Navigate(URLTextBox.Text);
+                BackLinks.Push(URLTextBox.Text);
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            if(BackLinks.Count > 0)
+            {
+                ForwardLinks.Push(URLTextBox.Text);
+                if (URLTextBox.Text == BackLinks.Peek())
+                    BackLinks.Pop();
+                URLTextBox.Text = BackLinks.Peek();
+                this.webBrowser1.Navigate(BackLinks.Pop());              
+            }
+        }
+
+        private void ForwardButton_Click_1(object sender, EventArgs e)
+        {
+            if (ForwardLinks.Count > 0)
+            {
+                BackLinks.Push(URLTextBox.Text);
+                URLTextBox.Text = ForwardLinks.Peek();
+                this.webBrowser1.Navigate(ForwardLinks.Pop());
+            }
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate(URLTextBox.Text);
+        }
+
+        //I cannot figure out how to get access to my TabControl class.
+        //There seems to be an issue with the visibility of the parent class (TabbedBroswer).
+        //Hopefully I can understand what is happening with this issue before next week's project is due.
+        private void FileNewTabButton_Click(object sender, EventArgs e)
+        {            
+            int counter = 2;
+            TabPage tp = new TabPage($"Tab {counter}");
+            
+            //tabControl.TabPages.Add(tp);
+            //TabbedBrowser.tabControl.TabPages.Add(tp);
+        }
+    }
+}
---
+++ WebBrowswer.UI/TabbedBrowser.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+using System.Drawing;
+
+namespace WebBrowswer.UI
+{
+    partial class TabbedBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TabbedBrowser));
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.browserBar1 = new WebBrowswer.UI.BrowserBar();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1223, 801);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.browserBar1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1215, 769);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tab 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // browserBar1
+            // 
+            this.browserBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.browserBar1.Location = new System.Drawing.Point(0, 0);
+            this.browserBar1.Margin = new System.Windows.Forms.Padding(1209729, 545481, 1209729, 545481);
+            this.browserBar1.Name = "browserBar1";
+            this.browserBar1.Size = new System.Drawing.Size(65535, 65535);
+            this.browserBar1.TabIndex = 0;
+            // 
+            // TabbedBrowser
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1223, 800);
+            this.Controls.Add(this.tabControl);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1239, 839);
+            this.Name = "TabbedBrowser";
+            this.Text = "Grooble Chrom";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage tabPage1;
+        private BrowserBar browserBar1;
+        public System.Windows.Forms.TabControl tabControl;
+    }
+}
+
